9a00942ef6c92b4a5b4474ceb8c49d2cfea9262d
kartikgoyal369 <kartikgoyal9352@gmail.com>
2026-06-30 18:38:14 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,50 @@
+while True:
+    num1 = float(input("enter the first number: "))
+    operation = input("choose(+,-,*,/,%): ")
+    num2 = float(input("enter the second number: "))
+    if operation == "/":
+        if num2 == 0:
+            print("can not divided by 0")
+            continue
+        result = num1/num2
+
+    if operation == "%":
+        result = num1%num2 
+
+
+    if operation == "*":
+        result = num1*num2 
+
+    if operation == "-":
+        result = num1 - num2 
+
+    if operation == "+":   
+        result = num1 + num2
+
+    else:
+        print("Invalide operation")
+        continue
+
+    print(f"the operation of {num1} and {num2} is {result}: ")
+
+
+    game = input("do you want to calculate again?(y/n): ")
+    if game.lower() == "y":
+        print("welcome to game")
+        continue
+    if game.lower() == "n":
+        print("byyyy")
+    else:
+        print("not a valid ans, so fuck you laterrrrr...") 
+        continue
+    break
+    
+    
+
+    
+           
+    
+    
+                    
+    
+
